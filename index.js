fbe471835b4b600f7b03b6336f1eb38d4ff231c9
Flavien Nannini <flaviennannini@macbook-pro-de-flavien-1.home>
2022-07-14 03:18:13 +0200

    mousemove

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const btn1 = document.getElementById("btn-1");
 const btn2 = document.getElementById("btn-2");
 const response = document.querySelector("p");
+const mousemove = document.querySelector(".mousemove");
 
 btn1.addEventListener("click", () => {
   response.classList.add("show-response");
@@ -10,4 +11,9 @@ btn1.addEventListener("click", () => {
 btn2.addEventListener("click", () => {
   response.classList.add("show-response");
   response.style.background = "red";
+});
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
 });
